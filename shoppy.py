db399e315d6ef72eb8a011f54b5ebbef59444493
Eric Werner <ewerybody+github@gmail.com>
2021-10-06 00:32:19 +0200

    Toolbar now with more & proper icons

--- shoppy.py
+++ shoppy.py
@@ -4,6 +4,7 @@ from PySide2 import QtCore, QtGui, QtWidgets
 
 NAME = 'Shoppy'
 import logging
+
 LOG_LEVEL = logging.DEBUG
 log = logging.getLogger(NAME)
 log.setLevel(LOG_LEVEL)
@@ -12,6 +13,9 @@ DIM_OPACITY = 150
 DIM_DURATION = 200
 DIM_INTERVAL = 20
 PATH = os.path.abspath(os.path.dirname(__file__))
+IMG_PATH = os.path.join(PATH, 'img')
+MODE_CAM = 'Image'
+MODE_VID = 'Video'
 
 
 class Shoppy(QtWidgets.QGraphicsView):
@@ -31,13 +35,16 @@ class Shoppy(QtWidgets.QGraphicsView):
         self.setBackgroundBrush(QtGui.QBrush(self.original_pixmap))
 
         self.dimmer = Dimmer(self)
+        QtCore.QTimer(self).singleShot(300, self.dimmer.dim)
 
         QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Escape), self, self.escape)
         for seq in QtCore.Qt.Key_S, QtCore.Qt.CTRL + QtCore.Qt.Key_S:
             QtWidgets.QShortcut(QtGui.QKeySequence(seq), self, self.save_shot)
+        for seq in QtCore.Qt.Key_C, QtCore.Qt.CTRL + QtCore.Qt.Key_C:
+            QtWidgets.QShortcut(QtGui.QKeySequence(seq), self, self.clip)
 
         self.toolbox = None
-        QtCore.QTimer(self).singleShot(300, self._build_toolbox)
+        QtCore.QTimer(self).singleShot(400, self._build_toolbox)
         self.settings = Settings()
         self.settings.loaded.connect(self._drag_last_tangle)
 
@@ -127,6 +134,8 @@ class Shoppy(QtWidgets.QGraphicsView):
     def _build_toolbox(self):
         self.toolbox = ToolBox(self)
         self.toolbox.close_requested.connect(self.escape)
+        self.toolbox.save.connect(self.save_shot)
+        self.toolbox.clip.connect(self.clip)
         self.activateWindow()
 
     def set_cursor(self, shape: QtCore.Qt.CursorShape):
@@ -144,10 +153,19 @@ class Shoppy(QtWidgets.QGraphicsView):
         if not file_path:
             return
 
+        self.dimmer.flash()
         cutout = self.original_pixmap.copy(self._dragtangle)
         cutout.save(file_path)
         self.settings.last_save_path = os.path.dirname(file_path)
+        self._save_rect()
 
+    def clip(self):
+        self.dimmer.flash()
+        cutout = self.original_pixmap.copy(self._dragtangle)
+        QtWidgets.QApplication.clipboard().setPixmap(cutout)
+        self._save_rect()
+
+    def _save_rect(self):
         rect_list = list(self._dragtangle.getRect())
         if rect_list in self.settings.last_rectangles:
             if self.settings.last_rectangles[-1] == rect_list:
@@ -175,15 +193,13 @@ class Dimmer(QtCore.QObject):
         self.r2 = QtWidgets.QGraphicsRectItem()
         self.r3 = QtWidgets.QGraphicsRectItem()
         self.r4 = QtWidgets.QGraphicsRectItem()
-        self.rects = (self.r1, self.r2, self.r3, self.r4)
-
-        self.color = QtGui.QColor()
+        self.rects = ()
+        self._set_outer()
         self.color.setAlpha(0)
         scene = parent.scene()
 
         for r in self.rects:
             scene.addItem(r)
-            # self.rect.setRect()
             r.setBrush(self.color)
             r.setPen(QtGui.QPen(QtCore.Qt.transparent, 0))
 
@@ -198,9 +214,6 @@ class Dimmer(QtCore.QObject):
         self._timer.timeout.connect(self._update)
         self._timer.setInterval(DIM_INTERVAL)
 
-        self.dim()
-        self.cutout(QtCore.QRect(0, 0, 0, 0))
-
     def cutout(self, rect: QtCore.QRect):
         rect = rect.normalized()
         gw, gh = self.geo.width(), self.geo.height()
@@ -211,14 +224,20 @@ class Dimmer(QtCore.QObject):
         self.r4.setRect(0, rh + rect.y(), gw, gh - rh - rect.y())
         self.rx.setRect(rect)
 
+    def _set_outer(self):
+        self.rects = (self.r1, self.r2, self.r3, self.r4)
+        self.color = QtGui.QColor(QtCore.Qt.black)
+
     def dim(self):
         self._ticks = DIM_DURATION / DIM_INTERVAL
         self._delta = DIM_OPACITY / self._ticks
         self._timer.start()
 
     def undim(self):
+        self._set_outer()
+        self.color.setAlpha(DIM_OPACITY)
         self._ticks = DIM_DURATION / DIM_INTERVAL
-        self._delta = -self.color.alpha() / self._ticks
+        self._delta = -DIM_OPACITY / self._ticks
         self._timer.start()
 
     def _update(self):
@@ -233,29 +252,66 @@ class Dimmer(QtCore.QObject):
         for r in self.rects:
             r.setBrush(self.color)
 
+    def flash(self):
+        self.rects = (self.rx,)
+        self.color = QtGui.QColor(QtCore.Qt.white)
+        self._ticks = DIM_DURATION / DIM_INTERVAL
+        self.color.setAlpha(100)
+        self._delta = -DIM_OPACITY / self._ticks
+        self._timer.start()
+
 
 class ToolBox(QtWidgets.QWidget):
     close_requested = QtCore.Signal()
+    mode_switched = QtCore.Signal(str)
+    save = QtCore.Signal()
+    clip = QtCore.Signal()
 
     def __init__(self, parent):
-        super(ToolBox, self).__init__(parent)
+        super().__init__(parent)
         layout = QtWidgets.QHBoxLayout(self)
-        button = QtWidgets.QToolButton()
-        x_button = QtWidgets.QToolButton()
-        x_button.clicked.connect(self.x)
-        layout.addWidget(button)
-        layout.addWidget(x_button)
+        layout.setContentsMargins(5, 5, 5, 5)
+        _TbBtn(self, img.save, self.save.emit)
+        _TbBtn(self, img.clipboard, self.clip.emit)
+        self.mode_button = _TbBtn(self, img.camera, self.toggle_mode)
+        self.settings_btn = _TbBtn(self, img.settings)
+        _TbBtn(self, img.x, self.x)
         self.setWindowFlags(QtCore.Qt.Window | QtCore.Qt.FramelessWindowHint)
+        self._mode = MODE_CAM
+        self.show()
 
+    def showEvent(self, event):
         toolgeo = self.geometry()
-        toolgeo.moveCenter(parent.geometry().center())
+        toolgeo.moveCenter(self.parent().geometry().center())
         toolgeo.moveTop(0)
         self.setGeometry(toolgeo)
-        self.show()
+        return super().showEvent(event)
+
+    def toggle_mode(self):
+        if self._mode == MODE_CAM:
+            self._mode = MODE_VID
+            self.mode_button.setIcon(img.video)
+        else:
+            self._mode = MODE_CAM
+            self.mode_button.setIcon(img.camera)
+        self.mode_switched.emit(self._mode)
 
     def x(self):
         self.close_requested.emit()
         self.hide()
+
+
+class _TbBtn(QtWidgets.QToolButton):
+    def __init__(self, parent, icon, func=None):
+        super().__init__(parent)
+        if func is None:
+            self.setEnabled(False)
+        else:
+            self.clicked.connect(func)
+        self.setIcon(icon)
+        self.setAutoRaise(True)
+        self.setIconSize(QtCore.QSize(48, 48))
+        parent.layout().addWidget(self)
 
 
 class Settings(QtCore.QObject):
@@ -309,6 +365,71 @@ class Settings(QtCore.QObject):
 
         with open(self._settings_path, 'w') as file_obj:
             json.dump(current, file_obj, indent=2, sort_keys=True)
+
+
+class _ImgStub:
+    """
+    Load-only-once image library object.
+
+    For convenience this already lists all usable icons and for speed it
+    just loads them up when actually needed.
+    """
+
+    def __init__(self):
+        self._blank = self._get_ico()
+        self.camera = self._blank
+        self.check = self._blank
+        self.clipboard = self._blank
+        self.crop = self._blank
+        self.down = self._blank
+        self.edit = self._blank
+        self.file = self._blank
+        self.film = self._blank
+        self.folder = self._blank
+        self.github = self._blank
+        self.info = self._blank
+        self.link = self._blank
+        self.maximize = self._blank
+        self.monitor = self._blank
+        self.move = self._blank
+        self.pen = self._blank
+        self.plus = self._blank
+        self.pointer = self._blank
+        self.question = self._blank
+        self.refresh = self._blank
+        self.save = self._blank
+        self.settings = self._blank
+        self.type = self._blank
+        self.update = self._blank
+        self.upload = self._blank
+        self.upload2 = self._blank
+        self.video = self._blank
+        self.x = self._blank
+
+    def __getattribute__(self, name):
+        try:
+            obj = super(_ImgStub, self).__getattribute__(name)
+        except AttributeError:
+            log.error('Icons lib got request for inexistent icon:\n  "%s"!', name)
+            return self._blank
+
+        if not name.startswith('_'):
+            if obj is self._blank:
+                icon = self._get_ico(name)
+                setattr(self, name, icon)
+                return icon
+        return obj
+
+    def _get_ico(self, name=''):
+        if name:
+            path = os.path.join(IMG_PATH, name + '.svg')
+            if os.path.isfile(path):
+                return QtGui.QIcon(path)
+            log.error('No such file: {path}')
+        return QtGui.QIcon()
+
+
+img = _ImgStub()
 
 
 if __name__ == '__main__':
